0fb818c9cf4935ca9cc35bfb1c22ca9d3e9b6ec1
RpMoreira98 <rodrigopontes126@gmail.com>
2023-10-12 12:15:03 -0300

    first commit

---
+++ script/index.js
@@ -0,0 +1,50 @@
+const button = document.querySelectorAll('.number');
+const clear = document.getElementById('clear');
+const back = document.querySelector('#back');
+const resultValue = document.getElementById('result');
+
+//Event of numbers appearing on the screen.
+button.forEach((e)=>{
+    e.addEventListener('click', ()=> {
+        ((num) => {
+            const value = document.getElementById('display');
+            value.innerHTML += num;
+        })(e.innerHTML);
+    })
+})
+
+//Clear screen event.
+clear.addEventListener('click', ()=> {
+    (() => {
+        const display = document.getElementById('display');
+        display.innerHTML = '';
+    })();
+})
+
+//Event of erasing a number from the screen.
+back.addEventListener('click', () => {
+    (() => {
+        const display = document.getElementById('display');
+        display.innerHTML = display.innerHTML ? display.innerHTML.slice(0, display.innerHTML.length - 1) : display.innerHTML = '';
+    })();
+});
+
+
+//Calculation event.
+resultValue.addEventListener('click', ()=> {
+    (() => {
+        let result = document.getElementById('display');
+        if (result) {
+            try {
+                result.innerHTML = eval(display.innerHTML);
+            } catch (error) {
+                result.innerHTML = 'ERRO';
+            }
+        };
+    })();
+})
+
+
+
+
+
